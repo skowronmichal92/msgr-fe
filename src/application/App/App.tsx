177760c6be0a468cfccd5883abd6233a0831af23
Michal Skowron <user.ylv@gmail.com>
2020-04-11 13:05:38 +0200

    initial setup

---
+++ src/application/App/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Routes } from '../Routes';
+
+export const App = () => {
+  return (
+    <React.StrictMode>
+      <Routes />
+    </React.StrictMode>
+  );
+};
---
+++ src/modules/index.ts
@@ -0,0 +1,3 @@
+export { Home } from './Home';
+export { LogIn } from './LogIn';
+export { SignUp } from './SignUp';
